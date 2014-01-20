a8dc2187b33cff301d8e37d00ee53d3e4c1586fb
Connor Cimowsky <connorcimowsky@me.com>
2014-01-20 12:04:24 -0500

    Rename node, list, queue to k_node, k_list, k_queue.

---
+++ Context_Switching/src/k_queue.h
@@ -0,0 +1,19 @@
+#ifndef K_QUEUE_H
+#define K_QUEUE_H
+
+#include "k_node.h"
+
+typedef struct k_queue_t {
+    k_node_t *first;
+    k_node_t *last;
+} k_queue_t;
+
+k_queue_t *new_queue(void);
+
+int enqueue_node(k_queue_t *queue, k_node_t *node);
+
+k_node_t *dequeue_node(k_queue_t *queue);
+
+int is_queue_empty(k_queue_t *queue);
+
+#endif
--- Context_Switching/src/node.h
+++
@@ -1,8 +0,0 @@
-#ifndef NODE_H
-#define NODE_H
-
-typedef struct node_t {
-    struct node_t *next;
-} node_t;
-
-#endif
