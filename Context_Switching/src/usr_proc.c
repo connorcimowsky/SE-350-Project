191e80f49f5ac5feed85ae06aab6dc4f16bd1d1a
Connor Cimowsky <connorcimowsky@me.com>
2014-01-27 18:10:52 -0500

    Clean up usr_proc.h and usr_proc.c.

--- Context_Switching/src/usr_proc.c
+++ Context_Switching/src/usr_proc.c
@@ -1,22 +1,16 @@
-/**
- * @file:   usr_proc.c
- * @brief:  Two user processes: proc1 and proc2
- * @author: Yiqing Huang
- * @date:   2014/01/17
- * NOTE: Each process is in an infinite loop. Processes never terminate.
- */
-
-#include "rtx.h"
-#include "uart_polling.h"
 #include "usr_proc.h"
+#include "uart_polling.h"
 
 #ifdef DEBUG_0
 #include "printf.h"
 #endif
 
+
+/* global variables */
 PROC_INIT g_test_procs[NUM_TEST_PROCS];
 
-void set_test_procs() {
+
+void set_test_procs(void) {
 	int i;
 	for( i = 0; i < NUM_TEST_PROCS; i++ ) {
 		g_test_procs[i].m_pid=(U32)(i+1);
@@ -28,11 +22,6 @@ void set_test_procs() {
 	g_test_procs[1].mpf_start_pc = &proc2;
 }
 
-
-/**
- * @brief: a process that prints five uppercase letters
- *         and then yields the cpu.
- */
 void proc1(void)
 {
 	int i = 0;
@@ -50,10 +39,6 @@ void proc1(void)
 	}
 }
 
-/**
- * @brief: a process that prints five numbers
- *         and then yields the cpu.
- */
 void proc2(void)
 {
 	int i = 0;
--- Context_Switching/src/usr_proc.h
+++ Context_Switching/src/usr_proc.h
@@ -1,17 +1,21 @@
-/**
- * @file:   usr_proc.h
- * @brief:  Two user processes header file
- * @author: Yiqing Huang
- * @date:   2014/01/17
- */
- 
 #ifndef USR_PROC_H
 #define USR_PROC_H
 
+#include "rtx.h"
+
+
+/* external functions */
 extern PROC_INIT g_test_procs[NUM_TEST_PROCS];  /* process initialization table */
 
+
+/* populate the table of test processes */
 void set_test_procs(void);
+
+/* prints five uppercase letters and then releases the processor */
 void proc1(void);
+
+/* prints five numbers and then releases the processor */
 void proc2(void);
+
 
 #endif /* USR_PROC_H */
