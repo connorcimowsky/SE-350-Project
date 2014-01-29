16122df8d60f00c432c877f546142dd66d5479d9
Connor Cimowsky <connorcimowsky@me.com>
2014-01-28 22:16:21 -0500

    Add the null process.

--- Context_Switching/src/usr_proc.c
+++ Context_Switching/src/usr_proc.c
@@ -10,17 +10,43 @@
 PROC_INIT g_test_procs[NUM_TEST_PROCS];
 
 
-void set_test_procs(void) {
-	int i;
-	for( i = 0; i < NUM_TEST_PROCS; i++ ) {
-		g_test_procs[i].m_pid=(U32)(i+1);
-		g_test_procs[i].m_priority=LOWEST;
-		g_test_procs[i].m_stack_size=0x100;
+void set_test_procs(void)
+{
+    int i;
+    for( i = 0; i < NUM_TEST_PROCS; i++ ) {
+        g_test_procs[i].m_pid = (U32)(i + 1);
 	}
-  
-	g_test_procs[0].mpf_start_pc = &proc1;
-	g_test_procs[1].mpf_start_pc = &proc2;
-	g_test_procs[2].mpf_start_pc = &proc3;
+    
+    g_test_procs[0].m_priority = LOWEST;
+    g_test_procs[0].m_stack_size = 0x100;
+    g_test_procs[0].mpf_start_pc = &null_process;
+    
+    g_test_procs[1].m_priority = LOW;
+    g_test_procs[1].m_stack_size = 0x100;
+    g_test_procs[1].mpf_start_pc = &proc1;
+    
+    g_test_procs[1].m_priority = MEDIUM;
+    g_test_procs[2].m_stack_size = 0x100;
+    g_test_procs[2].mpf_start_pc = &proc2;
+    
+    g_test_procs[1].m_priority = HIGHEST;
+    g_test_procs[3].m_stack_size = 0x100;
+    g_test_procs[3].mpf_start_pc = &proc3;
+}
+
+void null_process(void)
+{
+    int ret_val;
+    
+    while (1) {
+        
+        ret_val = release_processor();
+        
+#ifdef DEBUG_0
+        printf("null_process: ret_val = %d\n", ret_val);
+#endif
+        
+    }
 }
 
 void proc1(void)
