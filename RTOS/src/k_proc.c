65566f6c940ca8fc8c78ecc861f1bbcc76d8b6dc
Connor Cimowsky <connorcimowsky@me.com>
2014-02-24 11:05:15 -0500

    Initial implementation of timer i-process: non-blocking receive, enqueue in receipient's message queue, dispatch expired messages.

--- RTOS/src/k_proc.c
+++ RTOS/src/k_proc.c
@@ -35,7 +35,26 @@ __asm void TIMER0_IRQHandler(void)
 }
 
 void timer_i_process(void)
-{
+{   
+    msg_t *p_msg = (msg_t *)k_non_blocking_receive_message();
+    if (p_msg != NULL) {
+        U8 *p_decrement = (U8 *)p_msg;
+        p_decrement -= MSG_HEADER_OFFSET;
+        
+        queue_sorted_insert(&g_timeout_queue, (k_node_t *)p_decrement);
+    }
+    
+    while (!is_queue_empty(&g_timeout_queue) && queue_peek(&g_timeout_queue)->m_val <= g_timer_count) {
+        k_msg_t *p_next_message = (k_msg_t *)dequeue_node(&g_timeout_queue);
+        k_pcb_t *p_recipient_pcb = gp_pcbs[p_next_message->m_recipient_pid];
+    
+        if (enqueue_node(&(p_recipient_pcb->m_msg_queue), (k_node_t *)p_next_message) == RTOS_ERR) {
+#ifdef DEBUG_1
+            printf("%s: Could not send message to process %d\n\r", __FUNCTION__, p_recipient_pcb->m_pid);
+#endif
+        }
+    }
+    
     LPC_TIM0->IR = (1 << 0);
     
     g_timer_count++;
